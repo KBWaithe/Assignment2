35a300f452b08af8f7be3201394829dac5a30daf
Kris Waithe <KBWaithe@gmail.com>
2020-07-03 17:04:24 -0400

    updated pages

--- Scripts/app.js
+++ Scripts/app.js
@@ -9,14 +9,31 @@
     {
         console.log('%cApp Started...', "color:white; font-size: 24px;");
 
-        Second();
-    } 
+        let title = document.title;
 
-    function Second()
+        title = title.toLowerCase();
+
+        console.log(`The title of the page is ${title}`);
+        
+        let navAnchors = document.querySelectorAll("li a");
+        
+        for (const anchor of navAnchors) {
+
+            let anchorString = anchor.getAttribute("href");
+            anchorString = anchorString.substring(0,anchorString.length-5);
+
+        if((title === "home") && (anchorString === "index") || (title === anchorString))
     {
-        console.log("This is second function");
+        anchor.className = "nav-link active";
     }
 
+        console.log(anchorString);
+        }
+        
+
+    } 
+
+    
 
 
     window.addEventListener("load", Start);
