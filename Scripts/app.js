b7eaa45bf5a99dd6d2ca2889610e3cb95c336159
Kris Waithe <KBWaithe@gmail.com>
2020-07-03 17:45:35 -0400

    added form to Contact page

--- Scripts/app.js
+++ Scripts/app.js
@@ -7,7 +7,7 @@
     // named function
     function Start()
     {
-        console.log('%cApp Started...', "color:white; font-size: 24px;");
+          console.log('%cApp Started...', "color:white; font-size: 24px;");
 
         let title = document.title;
 
@@ -17,19 +17,28 @@
         
         let navAnchors = document.querySelectorAll("li a");
         
-        for (const anchor of navAnchors) {
+        for (const anchor of navAnchors) 
+        {
 
             let anchorString = anchor.getAttribute("href");
             anchorString = anchorString.substring(0,anchorString.length-5);
 
         if((title === "home") && (anchorString === "index") || (title === anchorString))
-    {
-        anchor.className = "nav-link active";
-    }
-
-        console.log(anchorString);
+         {
+            anchor.className = "nav-link active";
+         }
         }
-        
+        // Step 1. Hook into the element
+            let jumbotron = document.getElementsByClassName("jumbotron")[0];
+
+        // Step 2. Create a new element
+            let newParagraph = document.createElement("p");
+
+        // Step 3. Configure the new element
+            newParagraph.textContent = title;
+
+        // Step 4. Attach the new element
+        jumbotron.appendChild(newParagraph);
 
     } 
 
