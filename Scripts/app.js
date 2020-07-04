313625cf3133619545d87d52efd72f0fd81e80f0
Kris Waithe <KBWaithe@gmail.com>
2020-07-04 16:59:07 -0400

    js update

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,84 +1,48 @@
 // IIFE -Immediately Ivoked Function Expression
-(function(){
-
-    /* COMP125(Sec.004) - Assignment 2
+(function () {
+  /* COMP125(Sec.004) - Assignment 2
     Student Name - Kristian Waithe
     App.Js used for Mini Portfolio functionality    
     */
 
+  // named function
+  function Start() {
+    console.log("%cApp Started...", "color:white; font-size: 24px;");
 
-    // named function
-    function Start()
-    {
-          console.log('%cApp Started...', "color:white; font-size: 24px;");
+    let title = document.title;
 
-        let title = document.title;
+    title = title.toLowerCase();
 
-        title = title.toLowerCase();
+    console.log(`The title of the page is ${title}`);
 
-        console.log(`The title of the page is ${title}`);
-        
-        let navAnchors = document.querySelectorAll("li a");
-        
-        for (const anchor of navAnchors) 
-        {
+    let navAnchors = document.querySelectorAll("li a");
 
-            let anchorString = anchor.getAttribute("href");
-            anchorString = anchorString.substring(0,anchorString.length-5);
+    for (const anchor of navAnchors) {
+      let anchorString = anchor.getAttribute("href");
+      anchorString = anchorString.substring(0, anchorString.length - 5);
 
-        if((title === "home") && (anchorString === "index") || (title === anchorString))
-         {
-            anchor.className = "nav-link active";
-         }
-        }
-        // Step 1. Hook into the element
-            let jumbotron = document.getElementsByClassName("jumbotron")[0];
-
-            if(jumbotron)
-            {
-        // Adding content to landing page
-        aboutMe();
-        function aboutMe(){
-        
-            if(document.getElementById("myName"))  
-            
-            {
-                let myName = "Kris Waithe";
-                document.getElementById("myName").innerHTML = myName;
-            }
-    
-            if(document.getElementById("welcome"))  
-            
-            {
-                let welcomeMessage = "Welcome!";
-                document.getElementById("welcome").innerHTML = welcome;
-            }
-
-            if(document.getElementById("aboutText"))
-
-            {let aboutText = 
-                `I'm a second semester Software Engineering Tecnologies student looking to further my knowledge in the field of tech. 
-                I have always found interest in the tech side of everything and I look forward to learning much more as a student at Centennial College!
-                `;
-document.getElementById("aboutText").innerText = aboutText;
-}
-            
-
-        // Step 2. Create a new element
-            let newParagraph = document.createElement("p");
-
-        // Step 3. Configure the new element
-            newParagraph.textContent = title;
-
-        // Step 4. Attach the new element
-        jumbotron.appendChild(newParagraph);
+      if (
+        (title === "home" && anchorString === "index") ||
+        title === anchorString
+      ) {
+        anchor.className = "nav-link active";
+      }
     }
 
-    } 
-
-    
-
+    if (title === "About Me") {
+        aboutMe();
+    }
 
     window.addEventListener("load", Start);
+  }
+});
 
-}})
+function aboutMe() {
+    let myName = "Kris Waithe", welcomeMessage = "Welcome!", aboutText = `I'm a second semester Software Engineering Tecnologies student looking to further my knowledge in the field of tech. 
+    I have always found interest in the tech side of everything and I look forward to learning much more as a student at Centennial College!
+    `;
+
+    document.getElementById("myName").innerHTML = myName;
+    document.getElementById("welcome").innerHTML = welcomeMessage;
+    document.getElementById("aboutText").innerHTML = aboutText;
+}
