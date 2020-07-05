73e7d43d9ef74c1beb0e865ad58cf1a2eef54ddc
Kris Waithe <KBWaithe@gmail.com>
2020-07-04 20:35:26 -0400

    projects update

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,3 +1,44 @@
+// IIFE -Immediately Ivoked Function Expression
+(function () {
+    /* COMP125(Sec.004) - Assignment 2
+        Student Name - Kristian Waithe
+        App.Js used for Mini Portfolio functionality    
+        */
+      Start();
+  });
+
+  // named function
+function Start() {
+    console.log("%cApp Started...", "color:white; font-size: 24px;");
+  
+    let title = document.title;
+  
+    title = title.toLowerCase();
+  
+    console.log(`The title of the page is ${title}`);
+  
+    let navAnchors = document.querySelectorAll("li a");
+  
+    for (const anchor of navAnchors) {
+      let anchorString = anchor.getAttribute("href");
+      anchorString = anchorString.substring(0, anchorString.length - 5);
+  
+      if (
+        (title === "home" && anchorString === "index") ||
+        title === anchorString
+      ) {
+        anchor.className = "nav-link active";
+      }
+    }
+  
+    if (title === "About Me") {
+      writeAboutMe();
+    }
+  
+    window.addEventListener("load", Start);
+  }
+
+// Content for index (Landing / Bio) Page.
 writeAboutMe();
 function writeAboutMe(){
 if(document.getElementById("myName"))  
@@ -17,42 +58,31 @@ document.getElementById("aboutText").innerText = aboutText;
 }
 }
 
-// named function
-function Start() {
-  console.log("%cApp Started...", "color:white; font-size: 24px;");
-
-  let title = document.title;
-
-  title = title.toLowerCase();
-
-  console.log(`The title of the page is ${title}`);
-
-  let navAnchors = document.querySelectorAll("li a");
-
-  for (const anchor of navAnchors) {
-    let anchorString = anchor.getAttribute("href");
-    anchorString = anchorString.substring(0, anchorString.length - 5);
-
-    if (
-      (title === "home" && anchorString === "index") ||
-      title === anchorString
-    ) {
-      anchor.className = "nav-link active";
-    }
-  }
-
-  if (title === "About Me") {
-    writeAboutMe();
-  }
-
-  window.addEventListener("load", Start);
+// Content for Projects page to be broken into 3 parts
+mainProject();
+function mainProject(){
+if(document.getElementById("projects"))
+{
+  let projectHeading = "My Projects";
+ document.getElementById("projects").innerHTML = projectHeading;
 }
+/*Description of all the projects*/
+if(document.getElementById("projectsText"))
+{
+let projectDescription = 
+`1st Project - Guitar
+I'm a casual guitar player who's played many instruments since middle school. Recently my guitar colection has grown with the fender acoustic!
+I've dabbled in music since I was a child and music as well as playing instruments is a gigantic part of who it is I am. 
 
-// IIFE -Immediately Ivoked Function Expression
-(function () {
-  /* COMP125(Sec.004) - Assignment 2
-      Student Name - Kristian Waithe
-      App.Js used for Mini Portfolio functionality    
-      */
-    Start();
-});
+2nd Project - Streaming
+I've been a part time streamer for around 2 years now, first getting into it while competiting in a few online games. I've played competitively in multiple games over the years.
+I look forward to having a better schedule that will allow me to grow my community further.
+
+Third Project - Video Games
+I imagine it doesn't come as a surprise that someone who streams also partakes in video games.
+I've been playing games since I was a toddler when I got my first console (the NES).
+Since then I've jumped from platform to platform finally landing on PC gaming. 
+I'm also some what of a collector having memorabilia from all kinds of games and platforms!`;
+document.getElementById("projectsText").innerText = projectDescription;
+}
+}
